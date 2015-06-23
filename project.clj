5b04c2aafc12a3b3be2b28f2ae79e0f85d6b94a9
Christian Betz <christian.betz@performance-media.de>
2015-06-23 14:15:06 +0200

    changed project name and description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject issues "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject dekk "0.1.0-SNAPSHOT"
+  :description "A distributed project management system"
+  :url "https://github.com/gorillalabs/dekk"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
